9c58847feb0e66e53806ccfa487912acb864d967
Jakob Leben <jakob.leben@gmail.com>
2014-10-26 19:42:53 -0700

    apply GNU GPL v2

--- space.hpp
+++ space.hpp
@@ -1,3 +1,23 @@
+/*
+isl-cpp: C++ bindings to the ISL (Integer Set Library)
+
+Copyright (C) 2014  Jakob Leben <jakob.leben@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #ifndef ISL_CPP_SPACE_INCLUDED
 #define ISL_CPP_SPACE_INCLUDED
 
